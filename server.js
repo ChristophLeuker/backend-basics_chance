705020a901caf38f9e19984a9d6919e87547b6d3
Christoph Leuker <cleuker87@gmail.com>
2023-06-15 12:24:21 +0200

    fix randomize every refresh

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import { createServer } from "node:http";
 import Chance from "chance";
 
-const chance = new Chance();
-const name = chance.name();
-const age = chance.age();
-const profession = chance.profession();
-
 export const server = createServer((request, response) => {
+  const chance = new Chance();
   response.statusCode = 200;
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
   response.end(
     `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
   );
